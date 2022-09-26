5651504f730b6df86e5e010152ed9dc464b12f87
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-26 13:07:24 -0400

    commit tree ebc53671a84d05e24fff14a05f0e71340df07efa

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// fetch requests
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then (res => res.json())
+    .then (json => renderBooks(json))
 }
 
+//initial render
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
